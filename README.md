13dd9c9fd41756311d001939860815be4722205b
nic-chen <33000667+nic-chen@users.noreply.github.com>
2020-09-23 18:07:44 +0800

    feat: simulate to run plugin `redirect`
    * init
    
    * fix: respond by request handler
    
    * run redirect  as plugin
    
    * doc: add comments
    
    * doc: add comments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# envoy-apisix
+# run
+```
+cd compose && docker-compose up
+```
+
+# test
+```
+curl 127.0.0.1:8000/ -i
+```
---
+++ apisix/core/ctx.lua
@@ -0,0 +1,42 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one or more
+-- contributor license agreements.  See the NOTICE file distributed with
+-- this work for additional information regarding copyright ownership.
+-- The ASF licenses this file to You under the Apache License, Version 2.0
+-- (the "License"); you may not use this file except in compliance with
+-- the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+local _M = {version = 0.2}
+
+--- Note: envoy doesn't support context for lua currently. using a global var as ctx temporarily.
+--- TODO: need a better implement and more vars
+local var = {}
+function _M.set_vars_meta(ctx, handler)
+    table.clear(var)
+    local headers = handler:headers()
+    for key, value in pairs(headers) do
+        if key == ":authority" then
+            var.host = value
+        elseif key == ":path" then
+            var.request_uri = value
+        elseif key == ":method" then
+            var.method = value
+        elseif key == "x-forwarded-proto" then
+            var.schema = value
+        else
+            var[key] = value
+        end
+    end
+    ctx.var = var
+end
+
+
+return _M
