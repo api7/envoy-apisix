fcf83130db082e71ac8288faa121e905a7c41e76
nic-chen <johz@163.com>
2020-11-12 13:45:48 +0800

    feat: read metadata

--- apisix/entry.lua
+++ apisix/entry.lua
@@ -2,14 +2,15 @@ local core = require("apisix.core")
 local ctx = {}
 
 function envoy_on_request(request_handle)
-  ctx.test = "a"
   core.ctx.set_vars_meta(ctx, request_handle)
 
-  local metadata = handle:metadata()
+  local metadata = request_handle:metadata()
+  local foo = metadata:get("foo")
+
 
   local ok, redirect = pcall(require, "apisix.plugins.redirect")
   local phase_func = redirect.rewrite
-  local conf = {ret_code = 302, uri = "/redirected/path"}
+  local conf = {ret_code = 302, uri = "/redirected/path" .. foo}
   phase_func(conf, ctx, request_handle)
 end
 
@@ -17,6 +18,6 @@ function envoy_on_response(response_handle)
   if ctx and ctx.var then
     for key, value in pairs(ctx.var) do
       response_handle:logWarn("ctx key:" .. key .. " value: " .. value)
-    end                 
-  end  
+    end
+  end
 end
